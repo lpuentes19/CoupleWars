c20e816345c6a030ea56852dd57e3c9534f3710e
lpuentes19 <luey.puentes@gmail.com>
2017-08-21 17:06:03 -0600

    Completed the Login view in IB, need to connect all IBOutlets and Actions

---
+++ CupleWars/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  CupleWars
+//
+//  Created by Luis Puentes on 8/21/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
