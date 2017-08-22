a10cdb050a6554b551cc3517bb5268a62ebb5637
lpuentes19 <luey.puentes@gmail.com>
2017-08-22 17:24:44 -0600

    Almost have the Create Account view completed. Need to get border color to show red on the fields that do not have any texts.

--- CupleWars/CreateAccountViewController.swift
+++ CupleWars/CreateAccountViewController.swift
@@ -21,25 +21,52 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var invalidInfoLabel: UILabel!
     @IBAction func createAccountButtonTapped(_ sender: Any) {
         
-        guard let username = usernameTextField.text, !username.isEmpty,
-            let email = emailTextField.text, !email.isEmpty,
-            let password = passwordTextField.text, !password.isEmpty else {
-                
+        usernameTextField.layer.borderWidth = 0
+        emailTextField.layer.borderWidth = 0
+        passwordTextField.layer.borderWidth = 0
+        
+        guard let username = usernameTextField.text, !username.isEmpty else {
+            
             usernameTextField.layer.borderWidth = 1.5
             usernameTextField.layer.cornerRadius = 5
             usernameTextField.layer.borderColor = UIColor.red.cgColor
-                
+            
+            invalidInfoLabel.text = "* Please enter a valid Username, Email, and/or Password."
+            invalidInfoLabel.textColor = .red
+            invalidInfoLabel.textAlignment = .center
+            return
+        }
+        
+        guard let email = emailTextField.text, !email.isEmpty else {
+            
             emailTextField.layer.borderWidth = 1.5
             emailTextField.layer.cornerRadius = 5
             emailTextField.layer.borderColor = UIColor.red.cgColor
-                
+            
+            invalidInfoLabel.text = "* Please enter a valid Username, Email, and/or Password."
+            invalidInfoLabel.textColor = .red
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
             passwordTextField.layer.borderWidth = 1.5
             passwordTextField.layer.cornerRadius = 5
             passwordTextField.layer.borderColor = UIColor.red.cgColor
-                
-            invalidInfoLabel.text = "* Please enter valid Username, Email, and/or Password."
+            
+            invalidInfoLabel.text = "* Please enter a valid Username, Email, and/or Password."
             invalidInfoLabel.textColor = .red
             return
+        }
+        
+        if let password = passwordTextField.text?.characters {
+            if password.count < 8 {
+                passwordTextField.layer.borderWidth = 1.5
+                passwordTextField.layer.cornerRadius = 5
+                passwordTextField.layer.borderColor = UIColor.red.cgColor
+                
+                invalidInfoLabel.text = "* Password must contain at least 8 characters."
+                invalidInfoLabel.textColor = .red
+            }
         }
         
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
