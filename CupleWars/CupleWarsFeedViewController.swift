645126dd23d75cc52b6525c914fae9e1c562b649
lpuentes19 <luey.puentes@gmail.com>
2017-09-11 20:05:20 -0600

    Added a new VC for the feed and connected all appropriate outlets

---
+++ CupleWars/CupleWarsFeedViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CupleWarsFeedViewController.swift
+//  CupleWars
+//
+//  Created by Luis Puentes on 9/11/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class CupleWarsFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var postTextField: UITextField!
+}
