ab2f09aa18ba6a51bfbb819107c785ea324d681f
lpuentes19 <luey.puentes@gmail.com>
2017-10-17 17:05:18 -0600

    Changed the name of the app to Couple Wars

--- CupleWars/CupleWars-Bridging-Header.h
+++ CupleWars/CupleWars-Bridging-Header.h
@@ -1,14 +1,14 @@
 //
-//  CupleWars-Bridging-Header.h
-//  CupleWars
+//  CoupleWars-Bridging-Header.h
+//  CoupleWars
 //
-//  Created by Luis Puentes on 10/14/17.
+//  Created by Luis Puentes on 10/17/17.
 //  Copyright © 2017 LuisPuentes. All rights reserved.
 //
+
+//#ifndef CoupleWars_Bridging_Header_h
+//#define CoupleWars_Bridging_Header_h
 #import "ProgressHUD.h"
-//#ifndef CupleWars_Bridging_Header_h
-//#define CupleWars_Bridging_Header_h
-//
-//
-//#endif /* CupleWars_Bridging_Header_h */
+
+//#endif /* CoupleWars_Bridging_Header_h */
 
