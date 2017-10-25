e7721b6f199eef2d96f2668ae1392bd1c3db726a
lpuentes19 <luey.puentes@gmail.com>
2017-10-25 16:38:23 -0600

    Configured the EULA and the switch so that the users cannot create an account until they agree to the EULA

--- CupleWars/CreateAccountViewController.swift
+++ CupleWars/CreateAccountViewController.swift
@@ -19,6 +19,12 @@ class CreateAccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSwitch()
+        
+        createAccountButton.isEnabled = false
+        createAccountButton.backgroundColor = .black
+        createAccountButton.setTitleColor(.lightGray , for: .normal)
+        
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(PostsViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
@@ -27,16 +33,34 @@ class CreateAccountViewController: UIViewController {
         view.endEditing(true)
     }
     
+    func setupSwitch() {
+        licensingSwitch.isOn = false
+        licensingSwitch.onTintColor = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1.0)
+    }
+    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var invalidInfoLabel: UILabel!
+    @IBOutlet weak var createAccountButton: UIButton!
     @IBOutlet weak var licensingSwitch: UISwitch!
     
     @IBAction func licensingSwitchTouched(_ sender: Any) {
+        
+        if licensingSwitch.isOn {
+            licensingSwitch.isOn = false
+            createAccountButton.isEnabled = false
+            createAccountButton.backgroundColor = .black
+            createAccountButton.setTitleColor(.lightGray , for: .normal)
+        } else {
+            licensingSwitch.isOn = true
+            createAccountButton.isEnabled = true
+            createAccountButton.backgroundColor = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1.0)
+            createAccountButton.setTitleColor(.white , for: .normal)
+        }
     }
+    
     @IBAction func createAccountButtonTapped(_ sender: Any) {
-        ProgressHUD.show("Waiting...", interaction: false)
         
         usernameTextField.layer.borderWidth = 0
         emailTextField.layer.borderWidth = 0
@@ -139,7 +163,8 @@ class CreateAccountViewController: UIViewController {
             invalidInfoLabel.textColor = .red
             
         } else {
-
+            
+            ProgressHUD.show("Waiting...", interaction: false)
             Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
                 if let error = error {
                     ProgressHUD.showError("\(error.localizedDescription)")
